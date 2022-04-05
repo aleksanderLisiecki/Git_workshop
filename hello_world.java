a4fd3576f01858e8241363b5e8d53dce3d80ee0f
JakubSkrzypczak <87517877+JakubSkrzypczak@users.noreply.github.com>
2022-04-05 16:36:03 +0200

    Convlict_1

--- hello_world.java
+++ hello_world.java
@@ -3,7 +3,7 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
-        System.out.println("Hello, Kuba to .!"); 
+        System.out.println("Hello, Aleksander to .!"); 
         System.out.println("Hello, Kuba to .!"); ujj
         System.out.println("Hello, World!"); 
     }
